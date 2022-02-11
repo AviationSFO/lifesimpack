193a0c413530261fbd4428e00e9749002dcb11d7
AviationSFO <srw08sf@gmail.com>
2022-02-11 07:08:06 -0800

    add health status

--- src/lifesim/__init__.py
+++ src/lifesim/__init__.py
@@ -1,5 +1,4 @@
 # Life simulator module for python by Steven Weinstein on 2-9-2022 (Py ver >= 3.6.0)
-# Version v1.1.0
 class AyoUrExceptionallyExistent(Exception):
     pass
 class AyoUrFatBro(AyoUrExceptionallyExistent):
@@ -13,13 +12,14 @@ class AyoUrOldBro(AyoUrExceptionallyExistent):
 class AyoKilledADeadGuy(AyoUrExceptionallyExistent):
     pass
 class Person:
-    def __init__ (self, name, height, weight, age = 0, alive = True):
+    def __init__ (self, name, height, weight, age = 0, health = 100, alive = True):
         self.fullname = name
         self.firstname = name.split(" ")[0]
         self.lastname = name.split(" ")[1]
         self.age = age
         self.height = height
         self.weight = weight
+        self.health = health
         self.alive = alive
     def ageup (self):
         self.age = int(self.age)+1
@@ -37,12 +37,14 @@ class Person:
             raise AyoUrTallBro("ayo ur kinda tall tbh")
     def enfatten (self, interval = 1):
         self.weight = int(self.weight)+int(interval)
+        self.health -= 0.5
         if (self.weight > 69420):
             self.kill()
             raise AyoUrFatBro("ayo ur fat bro")
     def kill (self):
         if self.alive:
             self.alive = False
+            self.health = 0
         else:
             raise AyoKilledADeadGuy("AyoKilledADeadGuy")
         print(f"Oops, {self.fullname} is now dead. Have fun!")
--- src/tests/test.py
+++ src/tests/test.py
@@ -1,7 +1,10 @@
 import lifesim as ls
-import time
 zachary = ls.Person("Zachary Sakowitz", 60, 100, age = 13)
-while True:
-    zachary.ageup()
-    print(zachary.age)
-    time.sleep(0.0125)
+samson = ls.Person("Ari Samson", 58, 135, 12)
+samson.kill()
+dannyboi = ls.Person("Daniel Kavalerchik", 57, 140, 12)
+while True: 
+    dannyboi.grow()
+    dannyboi.ageup()
+    print(dannyboi.height)
+    print(dannyboi.age);
